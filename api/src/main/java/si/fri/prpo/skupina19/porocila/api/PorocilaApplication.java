dd348fe0d3993538bf43d56ef8b6d4288c956b63
phantomchicken <nino.brezac@gmail.com>
2020-12-10 21:35:02 +0100

    začetni skelet

---
+++ api/src/main/java/si/fri/prpo/skupina19/porocila/api/PorocilaApplication.java
@@ -0,0 +1,8 @@
+package si.fri.prpo.skupina19.porocila.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("v1")
+public class PorocilaApplication extends Application {
+}
